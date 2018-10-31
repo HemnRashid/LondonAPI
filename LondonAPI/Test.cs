c73bf480ce086fca1954b079b2249fd81c80eaae
Rashid, Adam <adam.rashid@seb.se>
2018-10-31 17:11:46 +0100

    delegate test

--- LondonAPI/Test.cs
+++ LondonAPI/Test.cs
@@ -7,5 +7,22 @@ namespace LondonAPI
 {
     public class Test
     {
+
+            public delegate string fullName(string namn, string efternamn);
+
+            public string t()
+            {
+                fullName user1 = fullNameFunc;
+
+
+                return user1("Adam", "Rashid");
+            }
+
+            public string fullNameFunc(string name,string efternamn)
+            {
+                return name + efternamn;
+            }
+
+
     }
 }
